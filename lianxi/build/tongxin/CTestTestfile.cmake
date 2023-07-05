b5d0f54cb7776f9c807843cffc80255c852cf241
Your Name <1745640128@qq.com>
2023-07-05 21:12:35 +0800

    wocaole

---
+++ lianxi/build/tongxin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/curry/lianxi/src/tongxin
+# Build directory: /home/curry/lianxi/build/tongxin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ lianxi/src/tongxin/scripts/dingyue.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python
+import rospy
+from std_msgs.msg import String
+def fun(xiaoxi):
+    rospy.loginfo("接受的消息为：%s",xiaoxi.data)
+if __name__=="__main__":
+    rospy.init_node("linser")
+    sub=rospy.Subscriber("wei",String,fun,queue_size=10)
+    rospy.spin()
